49e54a0bf5f1c410e6d2b6027f3070a9fe924f85
jeloneklibera <jan.piotrowski00@gmail.com>
2019-01-13 21:36:50 +0100

    troche zmian

--- frontend/src/app/content/employee-list/employee-list.component.ts
+++ frontend/src/app/content/employee-list/employee-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/statics/employee';
+import { EMPLOYEES } from 'src/app/statics/mEployee';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
+  private employeeList:Employee[]=EMPLOYEES;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
+    showDetails(id:number){
+      this.router.navigateByUrl("/employee/"+id);
+    }
+  
 
 }
